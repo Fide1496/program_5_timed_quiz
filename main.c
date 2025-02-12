e8b1b092a8630eca1757c58ceab66aab41971a88
faithie <faith@toyridge.com>
2025-02-12 10:27:43 -0600

    initial commit with main.c and txt files

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>  
+#include <unistd.h> 
+#include <sys/stat.h>
+#include <errno.h>
+#include <time.h>
+
+#define BUF_SIZE 1
+
+int main(){
+    
+}
