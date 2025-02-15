049c44f41f3124b8b2f2e043bf7ae568d247b15d
faith <faith@toyridge.com>
2025-02-15 07:46:29 -0600

    saturday morning work, sigaction not working yet

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <signal.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>  
@@ -6,8 +8,65 @@
 #include <errno.h>
 #include <time.h>
 
-#define BUF_SIZE 1
 
-int main(){
+#define BUF_SIZE 1024
+
+
+
+int checkError(int val, const char *msg) {
+    if (val == -1) {
+        perror(msg);
+        exit(EXIT_FAILURE);
+    }
+    return val;
+}
+void takeQuiz(){
+    printf("use later");
+}
+
+void signalHandler(int sig){
+
+}
+
+int main(int argc, char *argv[])
+{
+
+    struct sigaction sa;
+    struct itimerval it;
+
+    it.it_interval.tv_sec = 15;
+    it.it_interval.tv_usec = 0;
+    it.it_value.tv_sec = 1;
+    it.it_value.tv_usec = 0;
+
+    sa.sa_handler = signalHandler;
+    sa.sa_flags = 0;
+    sigempyset(&sa.sa_mask);
+
+    checkError(sigaction(SIGALRM, &sa, NULL), "sigaction");
+
+    checkError(setitimer(ITIMER_REAL, &it, NULL),"Set itimer");
+
+    while(1){
+        pause();
+        printf("testing pause");
+    }
+
+
+
+
+
+
+
+    const char *quest_file="quest.txt";
+    const char *ans_file="ans.txt";
+
+    int quest_fd = checkError(open(quest_file, O_RDONLY),"Qestion text file");
+    int ans_fd = checkError(open(ans_file, O_RDONLY),"Answer text file");
+
+
+    close(quest_fd);
+    close(ans_fd);
     
+    exit(EXIT_SUCCESS);
 }
